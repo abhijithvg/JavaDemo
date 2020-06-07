00db4c5f537a91e0d256a5350a961e77542da338
Abhijith V G <abhijithvg@gmail.com>
2020-06-07 22:08:54 +0530

    ReqNo3: Created NewMethod without error by user2

--- src/com/abhi/HelloDemo.java
+++ src/com/abhi/HelloDemo.java
@@ -2,10 +2,14 @@ package com.abhi;
 
 public class HelloDemo {
 	
-	public static void main(String[] args) {
-		
+	static void myMethod() {
 		System.out.println("Hello Demo from Eclipse!");
 		System.out.println("My second change");
+	}
+	
+	public static void main(String[] args) {
+		
+		myMethod();
 		
 	}
 
